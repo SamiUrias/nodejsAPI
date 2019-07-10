5565c2c902a9b7e21dd17104266d6fa1b4147808
Samuel Urias <msamuelurias@gmail.com>
2019-07-10 06:24:32 -0600

    Parsing Request Path

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hellow World');
+/**
+ * Primary file for the API
+ */
+
+ // Dependencies
+ const http =  require('http')
+ const url = require('url')
+
+// The server should respond to all request with a string
+var server = http.createServer(function(request, response){
+    
+    // Get the URL and parse it
+    var parsedUrl = url.parse(request.url, true);
+
+    // GEt the path
+    var path = parsedUrl.pathname; 
+    var trimmmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
+    response.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request is receivd on this path: ' +trimmmedPath);
+});
+
+
+ // Start the server, and have it listen on port 3000
+ server.listen(3000, function(){
+    console.log('The server is listening on port 3000 now');
+    console.log('Check it on: http://localhost:3000');
+ });
