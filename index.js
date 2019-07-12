a62caab2da06162692687e334cfbb0af9ad4f2cd
Samuel Urias <msamuelurias@gmail.com>
2019-07-12 09:14:16 -0600

    The path, queryString, HTTP method and headers are parsed now

--- index.js
+++ index.js
@@ -5,22 +5,36 @@
  // Dependencies
  const http =  require('http')
  const url = require('url')
+ const stringDecoder = require('string_decoder').stringDecoder
 
 // The server should respond to all request with a string
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(req, res){
     
     // Get the URL and parse it
-    var parsedUrl = url.parse(request.url, true);
+    var parsedUrl = url.parse(req.url, true);
 
     // GEt the path
     var path = parsedUrl.pathname; 
     var trimmmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the querystrings as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP Method
+    var method  = req.method.toLowerCase();
+
+    // Get the headers as and object
+    var headers = req.headers;
+
+
+
+
     // Send the response
-    response.end('Hello World\n');
+    res.end('Hello World\n');
 
     // Log the request path
-    console.log('Request is receivd on this path: ' +trimmmedPath);
+    console.log('Request is receivd on this path: ' + trimmmedPath + ' with method: ' + method +' with these query string paramters: ', queryStringObject); 
+    console.log('Request Headers: ', headers, '\n')
 });
 
 
